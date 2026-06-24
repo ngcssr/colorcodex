2b380b4ba49ddb4b7e69ccc44c68353dc7ff94f1
ngcssr <ngc2237ssr@gmail.com>
2026-06-24 19:29:41 +0800

    Replace site files

--- assets/js/mixer-core.js
+++ assets/js/mixer-core.js
@@ -1,2 +1,2 @@
 window.HCC_PAGE_ENTRY="mixer";
-(function(){var s=document.createElement('script');s.src='/assets/js/shared-core.js?v=20260624-193000';s.defer=true;document.body.appendChild(s)})();
+(function(){var s=document.createElement('script');s.src='/assets/js/shared-core.js?v=20260624-203000';s.defer=true;document.body.appendChild(s)})();
