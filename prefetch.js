d7b470852e85bd61061c86531de4f981dba6b7f8
ngcssr <ngc2237ssr@gmail.com>
2026-06-24 22:46:54 +0800

    Replace site files

--- prefetch.js
+++ prefetch.js
@@ -10,7 +10,7 @@
     '/contrast-checker/',
     '/color-mixer/'
   ];
-  var chunks=['/pages-extra.js?v=20260624-221500'];
+  var chunks=['/pages-extra.js?v=20260624-223000'];
   function hint(url,as){
     if(document.querySelector('link[href="'+url+'"]'))return;
     var l=document.createElement('link');
