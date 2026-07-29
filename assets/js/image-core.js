3435223562d81b52d65e9d2daf7537bd9912d47d
ngcssr <ngc2237ssr@gmail.com>
2026-07-29 19:07:20 +0800

    Replace site files

--- assets/js/image-core.js
+++
@@ -1,1 +0,0 @@
-window.HCC_PAGE_ENTRY="image",function(){var e=document.createElement("script");e.src="/assets/js/image-tools-core.js?v=20260629-103000",e.defer=!0,document.body.appendChild(e)}();
--- assets/js/wheel-core.js
+++
@@ -1,1 +0,0 @@
-window.HCC_PAGE_ENTRY="wheel",function(){var e=document.createElement("script");e.src="/assets/js/wheel-tools-core.js?v=20260629-103000",e.defer=!0,document.body.appendChild(e)}();
